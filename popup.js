0d609f109734535baf391e7acd5f0cadc84f6fe2
Liyana <32006266+liyanalzm@users.noreply.github.com>
2020-02-11 21:08:32 +0800

    First commit

---
+++ popup.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+
+				
+	function submitShow()
+	{
+		alert("Thank you ! Your form have been submitted, I'll get back to you later. :)");
+	}
+	function resetShow()
+	{
+		alert("Form already reset !");
+	}
